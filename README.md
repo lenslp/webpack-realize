4f7cf61742910ed557f03e5d14d8edc7b9a8f819
lens <18937708209@163.com>
2020-10-09 23:13:59 +0800

    手写webpack

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webpack-realize
-手写webpack
+
+手写 webpack
+
+## 文件夹
+
+- bin 源码
+  - my-pack.js
+- demo 示例
+
+## npm link 链接 my-pack 到全局
---
+++ bin/my-pack.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+// 1、拿到webpack.config.js配置文件
+let path = require("path");
+let config = require(path.resolve("webpack.config.js"));
+// 编译
+let Compiler = require("../lib/compiler");
+let compiler = new Compiler(config);
+compiler.run();
